d9c4b2054a6d5bf18b4100461b1b40f761a8bd82
Ronald <rpalacioso12@gmail.com>
2022-08-16 12:47:16 -0300

    fix(api and views): fixed (e) on server login

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import productsRouter from "./v1/routes/productsRoutes";
 import userRouter from './v1/routes/userRoutes.js';
+import morgan from "morgan";
 
 const app = express();
 
+//express use*//
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
+app.use(morgan('dev'))
+
+//routes//
 app.use("/api/products", productsRouter);
 app.use('/api/user', userRouter);
 
